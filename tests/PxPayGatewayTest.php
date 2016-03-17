b469d77bb25b20fb455d35358e25999e60714749
Alex Osborn <alex@elleander.co.nz>
2016-03-18 02:39:49 +1300

    Added transaction data methods to authorize request.

--- tests/PxPayGatewayTest.php
+++ tests/PxPayGatewayTest.php
@@ -44,6 +44,32 @@ class PxPayGatewayTest extends GatewayTestCase
         $this->assertSame('Invalid Key', $response->getMessage());
     }
 
+    public function testAuthorizeWithTransactionDataSuccess()
+    {
+        $this->setMockHttpResponse('PxPayPurchaseSuccess.txt');
+
+        $options = array_merge($this->options, [
+            'transactionData1' => 'Business Name',
+            'transactionData2' => 'Business Phone',
+            'transactionData3' => 'Business ID',
+        ]);
+
+        $request = $this->gateway->authorize($options);
+
+        $this->assertSame($options['transactionData1'], $request->getTransactionData1());
+        $this->assertSame($options['transactionData2'], $request->getTransactionData2());
+        $this->assertSame($options['transactionData3'], $request->getTransactionData3());
+
+        $response = $request->send();
+
+        $this->assertFalse($response->isSuccessful());
+        $this->assertTrue($response->isRedirect());
+        $this->assertNull($response->getTransactionReference());
+        $this->assertNull($response->getMessage());
+        $this->assertSame('https://sec.paymentexpress.com/pxpay/pxpay.aspx?userid=Developer&request=v5H7JrBTzH-4Whs__1iQnz4RGSb9qxRKNR4kIuDP8kIkQzIDiIob9GTIjw_9q_AdRiR47ViWGVx40uRMu52yz2mijT39YtGeO7cZWrL5rfnx0Mc4DltIHRnIUxy1EO1srkNpxaU8fT8_1xMMRmLa-8Fd9bT8Oq0BaWMxMquYa1hDNwvoGs1SJQOAJvyyKACvvwsbMCC2qJVyN0rlvwUoMtx6gGhvmk7ucEsPc_Cyr5kNl3qURnrLKxINnS0trdpU4kXPKOlmT6VacjzT1zuj_DnrsWAPFSFq-hGsow6GpKKciQ0V0aFbAqECN8rl_c-aZWFFy0gkfjnUM4qp6foS0KMopJlPzGAgMjV6qZ0WfleOT64c3E-FRLMP5V_-mILs8a', $response->getRedirectUrl());
+        $this->assertSame('GET', $response->getRedirectMethod());
+    }
+
     public function testPurchaseSuccess()
     {
         $this->setMockHttpResponse('PxPayPurchaseSuccess.txt');
