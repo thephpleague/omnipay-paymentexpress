314f1632342b3d3a3b04d56a1bfaa0b482e654e8
Geoffrey Hughes <geoffrey.hughes@otago.ac.nz>
2016-04-21 08:35:36 +1200

    Add conditionals for optional fields
    MerchantReference, TxnId, TxnData1, TxnData2, and TxnData3 are all optional fields so instead of sending empty strings if they're not set, don't include them in the request at all.

--- src/Message/PxPostAuthorizeRequest.php
+++ src/Message/PxPostAuthorizeRequest.php
@@ -124,11 +124,26 @@ class PxPostAuthorizeRequest extends AbstractRequest
         $data = $this->getBaseData();
         $data->InputCurrency = $this->getCurrency();
         $data->Amount = $this->getAmount();
-        $data->MerchantReference = $this->getDescription();
-        $data->TxnId = $this->getTransactionId();
-        $data->TxnData1 = $this->getTransactionData1();
-        $data->TxnData2 = $this->getTransactionData2();
-        $data->TxnData3 = $this->getTransactionData3();
+
+        if ($this->getDescription()) {
+            $data->MerchantReference = $this->getDescription();
+        }
+
+        if ($this->getTransactionId()) {
+            $data->TxnId = $this->getTransactionId();
+        }
+
+        if ($this->getTransactionData1()) {
+            $data->TxnData1 = $this->getTransactionData1();
+        }
+
+        if ($this->getTransactionData2()) {
+            $data->TxnData2 = $this->getTransactionData2();
+        }
+
+        if ($this->getTransactionData3()) {
+            $data->TxnData3 = $this->getTransactionData3();
+        }
 
         if ($this->getCardReference()) {
             $data->DpsBillingId = $this->getCardReference();
