960de478454a4a569bb2d8a99f5bb25b44e151c8
Geoffrey Hughes <geoffrey.hughes@otago.ac.nz>
2016-04-21 08:39:26 +1200

    Add conditionals for optional fields to PxPay request
    MerchantReference, TxnId, TxnData1, TxnData2, and TxnData3 are all optional fields so instead of sending empty strings if they're not set, don't include them in the request at all.

--- src/Message/PxPayAuthorizeRequest.php
+++ src/Message/PxPayAuthorizeRequest.php
@@ -179,15 +179,30 @@ class PxPayAuthorizeRequest extends AbstractRequest
         $data->PxPayUserId = $this->getUsername();
         $data->PxPayKey = $this->getPassword();
         $data->TxnType = $this->action;
-        $data->TxnId = $this->getTransactionId();
         $data->AmountInput = $this->getAmount();
         $data->CurrencyInput = $this->getCurrency();
-        $data->MerchantReference = $this->getDescription();
-        $data->TxnData1 = $this->getTransactionData1();
-        $data->TxnData2 = $this->getTransactionData2();
-        $data->TxnData3 = $this->getTransactionData3();
         $data->UrlSuccess = $this->getReturnUrl();
         $data->UrlFail = $this->getReturnUrl();
+
+        if ($this->getDescription()) {
+            $data->MerchantReference = $this->getDescription();
+        }
+
+        if ($this->getTransactionId()) {
+            $data->TxnId = $this->getTransactionId();
+        }
+
+        if ($this->getTransactionData1()) {
+            $data->TxnData1 = $this->getTransactionData1();
+        }
+
+        if ($this->getTransactionData2()) {
+            $data->TxnData2 = $this->getTransactionData2();
+        }
+
+        if ($this->getTransactionData3()) {
+            $data->TxnData3 = $this->getTransactionData3();
+        }
 
         if ($this->getCardReference()) {
             $data->DpsBillingId = $this->getCardReference();
