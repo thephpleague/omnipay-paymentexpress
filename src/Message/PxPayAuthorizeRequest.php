b77933302c5de336940a546b2b29eb30a4d3702a
Neil Bertram <neil@catalyst.net.nz>
2018-05-29 13:05:51 +1200

    Add missing setBillingId() and getBillingId() methods to the PxPayAuthorizeRequest

--- src/Message/PxPayAuthorizeRequest.php
+++ src/Message/PxPayAuthorizeRequest.php
@@ -178,6 +178,30 @@ class PxPayAuthorizeRequest extends AbstractRequest
     }
 
     /**
+     * Get the PxPay BillingId where this is used instead of the DpsBillingId
+     * for storing or retrieving a saved card profile.
+     *
+     * @see getCardReference() for DpsBillingId
+     * @return mixed
+     */
+    public function getBillingId()
+    {
+        return $this->getParameter('billingId');
+    }
+
+    /**
+     * Set a BillingId for use of a stored card with a local reference as an
+     * alternative to using cardReference (which uses DPS' generated reference instead)
+     *
+     * @param string $value Max 32 bytes
+     * @return $this
+     */
+    public function setBillingId($value)
+    {
+        return $this->setParameter('billingId', $value);
+    }
+
+    /**
      * Get the transaction data
      *
      * @return SimpleXMLElement
